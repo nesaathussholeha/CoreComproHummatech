2cda2b95422c2494c7c3f92ed49520f5a174dfde
Yudas <abdulkader0126@gmail.com>
2024-05-30 14:03:10 +0700

    Update index.blade.php

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -9,139 +9,20 @@
                 <div class="uk-container">
                     <div class="uk-grid-large uk-flex-middle" data-uk-grid>
                         <div class="uk-width-1-2@s in-slide-text">
-                            <p class="in-badge-text uk-text-small uk-margin-remove-bottom uk-visible@m"><span
-                                    class="uk-label uk-label-success in-label-small">New</span>Trade the markets
-                                directly with
-                                leading trading platforms.</p>
-                            <h1 class="uk-heading-small">The world's most <span
-                                    class="in-highlight">powerful</span> trade app.</h1>
-                            <p class="uk-text-lead uk-visible@m">Get the most accurate market data, alerts,
-                                conversions, tools and more — all within the same app.</p>
-                            <div class="uk-grid-medium uk-child-width-1-3@m uk-child-width-1-2@s uk-margin-medium-top uk-visible@s"
-                                data-uk-grid>
-                                <div>
-                                    <div
-                                        class="uk-card uk-card-small uk-card-secondary uk-card-body uk-border-rounded uk-flex uk-flex-middle">
-                                        <div class="in-symbol-logo">
-                                            <img src="assets_landing/img/in-lazy.gif"
-                                                data-src="assets_landing/img/in-symbol-tesla.svg" alt="ticker"
-                                                width="28" height="28" data-uk-img>
-                                        </div>
-                                        <div class="in-price down">
-                                            <h6 class="uk-margin-remove">TSLA<span
-                                                    class="uk-text-small">-1.47%</span></h6>
-                                            <p class="uk-margin-remove"><span
-                                                    class="fas fa-arrow-circle-right fa-xs"></span>$113.06</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div
-                                        class="uk-card uk-card-small uk-card-secondary uk-card-body uk-border-rounded uk-flex uk-flex-middle">
-                                        <div class="in-symbol-logo">
-                                            <img src="assets_landing/img/in-lazy.gif"
-                                                data-src="assets_landing/img/in-symbol-google.svg"
-                                                alt="ticker" width="28" height="28" data-uk-img>
-                                        </div>
-                                        <div class="in-price up">
-                                            <h6 class="uk-margin-remove">GOOGL<span
-                                                    class="uk-text-small">1.32%</span></h6>
-                                            <p class="uk-margin-remove"><span
-                                                    class="fas fa-arrow-circle-right fa-xs"></span>$87.34</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="uk-visible@m">
-                                    <div
-                                        class="uk-card uk-card-small uk-card-secondary uk-card-body uk-border-rounded uk-flex uk-flex-middle">
-                                        <div class="in-symbol-logo">
-                                            <img src="assets_landing/img/in-lazy.gif"
-                                                data-src="assets_landing/img/in-symbol-apple.svg" alt="ticker"
-                                                width="28" height="28" data-uk-img>
-                                        </div>
-                                        <div class="in-price up">
-                                            <h6 class="uk-margin-remove">AAPL<span
-                                                    class="uk-text-small">3.68%</span></h6>
-                                            <p class="uk-margin-remove"><span
-                                                    class="fas fa-arrow-circle-right fa-xs"></span>$129.62</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <h1 class="uk-heading-small"><span
+                                    class="in-highlight">Maju Bersama Berkembang Bersama</span></h1>
+                            <p class="uk-text-lead uk-visible@m">bertransformasi menjadi perusahaan yang mampu menjawab
+                                tantangan di era revolusi industri 4.0.</p>
+                               <div class="">
+                                <button class="uk-button uk-button-primary uk-border-rounded" style="background-color:#FCB42D; color:">
+                                    Selengkapnya <i class="fas fa-arrow-circle-right uk-margin-small-left"></i>
+                                </button>
+                               </div>
                         </div>
                         <div class="in-slide-img">
                             <img src="{{ asset('assets_landing/img/in-lazy.gif') }}"
                                 data-src="{{ asset('assets_landing/img/in-equity-slide-1.png') }}"
-                                alt="image-slide" width="652" height="746" data-uk-img>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="uk-flex uk-flex-middle">
-                <div class="uk-container">
-                    <div class="uk-grid-large uk-flex-middle" data-uk-grid>
-                        <div class="uk-width-1-2@s in-slide-text">
-                            <p class="in-badge-text uk-text-small uk-margin-remove-bottom uk-visible@m"><span
-                                    class="uk-label uk-label-success in-label-small">New</span>Trade the
-                                markets directly with
-                                leading trading platforms.</p>
-                            <h1 class="uk-heading-small">Reach out to new trading <span
-                                    class="in-highlight">experience</span>.</h1>
-                            <p class="uk-text-lead uk-visible@m">Bring your trading ventures go around the
-                                world, way beyond the space of your trading account.</p>
-                            <div class="uk-grid-medium uk-child-width-1-3@m uk-child-width-1-2@s uk-margin-medium-top uk-visible@s"
-                                data-uk-grid>
-                                <div>
-                                    <div
-                                        class="uk-card uk-card-small uk-card-secondary uk-card-body uk-border-rounded uk-flex uk-flex-middle">
-                                        <div class="in-symbol-logo">
-                                            <img src="img/in-lazy.gif" data-src="img/in-symbol-mcdonalds.svg"
-                                                alt="ticker" width="28" height="28" data-uk-img>
-                                        </div>
-                                        <div class="in-price down">
-                                            <h6 class="uk-margin-remove">MCD<span
-                                                    class="uk-text-small">-1.29%</span></h6>
-                                            <p class="uk-margin-remove"><span
-                                                    class="fas fa-arrow-circle-right fa-xs"></span>$269.47</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div
-                                        class="uk-card uk-card-small uk-card-secondary uk-card-body uk-border-rounded uk-flex uk-flex-middle">
-                                        <div class="in-symbol-logo">
-                                            <img src="img/in-lazy.gif" data-src="img/in-symbol-amazon.svg"
-                                                alt="ticker" width="28" height="28" data-uk-img>
-                                        </div>
-                                        <div class="in-price up">
-                                            <h6 class="uk-margin-remove">AMZN<span
-                                                    class="uk-text-small">3.56%</span></h6>
-                                            <p class="uk-margin-remove"><span
-                                                    class="fas fa-arrow-circle-right fa-xs"></span>$86.08</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="uk-visible@m">
-                                    <div
-                                        class="uk-card uk-card-small uk-card-secondary uk-card-body uk-border-rounded uk-flex uk-flex-middle">
-                                        <div class="in-symbol-logo">
-                                            <img src="img/in-lazy.gif" data-src="img/in-symbol-microsoft.svg"
-                                                alt="ticker" width="28" height="28" data-uk-img>
-                                        </div>
-                                        <div class="in-price down">
-                                            <h6 class="uk-margin-remove">MSFT<span
-                                                    class="uk-text-small">-1.18%</span></h6>
-                                            <p class="uk-margin-remove"><span
-                                                    class="fas fa-arrow-circle-right fa-xs"></span>$224.93</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="in-slide-img">
-                            <img src="assets_landing/img/in-lazy.gif"
-                                data-src="assets_landing/img/in-equity-slide-2.png" alt="image-slide"
-                                width="652" height="746" data-uk-img>
+                                alt="image-slide" width="500" height="746" data-uk-img>
                         </div>
                     </div>
                 </div>
@@ -156,11 +37,34 @@
 </div>
 <!-- slideshow content end -->
 <!-- section content begin -->
+<div class="uk-section in-equity-3 in-offset-top-65">
+    <div class="uk-container uk-margin-large-bottom">
+        <div class="uk-grid uk-flex uk-flex-middle">
+            <div class="uk-width-expand@m">
+                <h2 class="uk-margin-small-bottom"><span
+                        class="in-highlight">Menghadirkan Solusi Terintegrasi untuk Masa Digital</span></h2>
+                <p class="uk-margin-top uk-text-warning">Kini Hummatech bertransformasi menjadi perusahaan yang mampu menjawab tantangan di era revolusi industri 4.0 dengan menciptakan produk berbasis
+                    integrated system berupa perangkat lunak, seperti </p>
+                <hr class="uk-margin-medium-top uk-margin-medium-bottom">
+                <button class="uk-button uk-button-primary uk-border-rounded" style="background-color:#FCB42D; color:">
+                    Selengkapnya <i class="fas fa-arrow-circle-right uk-margin-small-left"></i>
+                </button>
+            </div>
+            <div class="uk-width-2xlarge uk-flex uk-flex-right uk-flex-center@s">
+                <div class="">
+                    <img src="{{ asset('assets/images/Logo_HUMMATECH_Icon.ico') }}" style="width:400px;" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- section content end -->
+<!-- section content begin -->
 <div class="uk-section uk-section-primary uk-preserve-color in-equity-1">
     <div class="uk-container">
         <div class="uk-grid">
             <div class="uk-width-1-1">
-                <h4>Popular products</h4>
+                <h4>Layanan Kami</h4>
             </div>
         </div>
         <div class="uk-grid-match uk-grid-medium uk-child-width-1-4@m uk-child-width-1-2@s uk-margin-bottom"
@@ -219,84 +123,7 @@
     </div>
 </div>
 <!-- section content end -->
-<!-- section content begin -->
-<div class="uk-section in-equity-2 uk-background-contain uk-background-center"
-    data-src="img/in-equity-2-bg.png" data-uk-img>
-    <div class="uk-container uk-margin-top">
-        <div class="uk-grid uk-flex uk-flex-center">
-            <div class="uk-width-2xlarge@m uk-text-center">
-                <span class="uk-label uk-label-warning">Fast execution, low latency<i
-                        class="fas fa-arrow-right fa-xs uk-margin-small-left"></i></span>
-                <h1 class="uk-margin-top">Your premium choice for trading currencies & stocks online</h1>
-                <p class="uk-text-lead uk-margin-medium-top">Harness the power of technology to make a quicker,
-                    smarter and more precise decision on CFD currency pairs, stocks, commodities and more</p>
-            </div>
-            <div class="uk-width-3-4@m uk-margin-medium-top">
-                <img class="uk-align-center" src="img/in-lazy.gif" data-src="img/in-equity-2-img.png"
-                    alt="image" width="758" height="334" data-uk-img>
-            </div>
-            <div class="uk-width-2xlarge@m uk-margin-medium-top">
-                <div class="uk-grid uk-child-width-1-4@m uk-child-width-1-4@s uk-text-center in-feature-box"
-                    data-uk-grid>
-                    <a href="#">
-                        <span class="in-icon-wrap">
-                            <img src="img/in-lazy.gif" data-src="img/in-equity-2-icon-1.svg" alt="icon-1"
-                                width="35" height="42" data-uk-img>
-                        </span>
-                        <p class="uk-margin-top">Trading calculators</p>
-                    </a>
-                    <a href="#">
-                        <span class="in-icon-wrap">
-                            <img src="img/in-lazy.gif" data-src="img/in-equity-2-icon-2.svg" alt="icon-2"
-                                width="38" height="42" data-uk-img>
-                        </span>
-                        <p class="uk-margin-top">Market analysis</p>
-                    </a>
-                    <a href="#">
-                        <span class="in-icon-wrap">
-                            <img src="img/in-lazy.gif" data-src="img/in-equity-2-icon-3.svg" alt="icon-3"
-                                width="42" height="42" data-uk-img>
-                        </span>
-                        <p class="uk-margin-top">Market reviews</p>
-                    </a>
-                    <a href="#">
-                        <span class="in-icon-wrap">
-                            <img src="img/in-lazy.gif" data-src="img/in-equity-2-icon-4.svg" alt="icon-4"
-                                width="42" height="42" data-uk-img>
-                        </span>
-                        <p class="uk-margin-top">Trading academy</p>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- section content end -->
-<!-- section content begin -->
-<div class="uk-section in-equity-3 in-offset-top-20">
-    <div class="uk-container uk-margin-large-bottom">
-        <div class="uk-grid uk-flex uk-flex-middle">
-            <div class="uk-width-expand@m">
-                <h1 class="uk-margin-small-bottom">Tight spreads and <span
-                        class="in-highlight">ultra-fast</span> execution</h1>
-                <h3 class="uk-margin-top uk-text-warning">Best market prices available so you can receive
-                    excellent conditions.</h3>
-                <hr class="uk-margin-medium-top uk-margin-medium-bottom">
-                <ul class="uk-list in-list-check">
-                    <li>Negative balance protection</li>
-                    <li>Segregated and supervised client funds</li>
-                    <li>Instant deposit & fast withdrawal</li>
-                </ul>
-            </div>
-            <div class="uk-width-2xlarge uk-flex uk-flex-right uk-flex-center@s">
-                <div class="uk-card uk-card-body uk-card-default uk-border-rounded in-margin-top-60@s">
-                    <div id="tradingview-widget"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- section content end -->
+
 <!-- section content begin -->
 <div class="uk-section in-equity-4">
     <div class="uk-container uk-margin-top uk-margin-medium-bottom">
